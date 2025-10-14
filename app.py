ce61dfbd5a726fcdc57788a7c5a18db67a81fae1
KuaaPoH <trankenn04@gmail.com>
2025-10-14 22:26:02 +0700

    flask

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from threading import Timer
+import webbrowser
+from werkzeug.serving import is_running_from_reloader
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+def _open():
+    webbrowser.open_new("http://127.0.0.1:5000")
+
+if __name__ == "__main__":
+    if not is_running_from_reloader():
+        Timer(1, _open).start()
+
+    app.run(debug=True)
